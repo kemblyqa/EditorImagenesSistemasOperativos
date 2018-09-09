c9215a6a2c9417b7003dd26917a3c13780bb2302
Eliomar Rodríguez <eliomar@estudiantec.cr>
2018-09-09 00:18:48 -0600

    Carga imagen
    Ya carga una imagen seleccionada por el usuario

--- EditorImagenes_Proyecto1/EditorImagenes_Proyecto1/Form1.Designer.cs
+++ EditorImagenes_Proyecto1/EditorImagenes_Proyecto1/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imgLoader1 = new System.Windows.Forms.PictureBox();
+            this.imgLoader2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLoader1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLoader2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgLoader1
+            // 
+            this.imgLoader1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgLoader1.Location = new System.Drawing.Point(16, 55);
+            this.imgLoader1.Name = "imgLoader1";
+            this.imgLoader1.Size = new System.Drawing.Size(463, 328);
+            this.imgLoader1.TabIndex = 0;
+            this.imgLoader1.TabStop = false;
+            // 
+            // imgLoader2
+            // 
+            this.imgLoader2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgLoader2.Location = new System.Drawing.Point(695, 55);
+            this.imgLoader2.Name = "imgLoader2";
+            this.imgLoader2.Size = new System.Drawing.Size(463, 328);
+            this.imgLoader2.TabIndex = 1;
+            this.imgLoader2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Imagen 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(691, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Imagen 2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(522, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 67);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cargar imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Imagen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1310, 681);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgLoader2);
+            this.Controls.Add(this.imgLoader1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgLoader1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLoader2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imgLoader1;
+        private System.Windows.Forms.PictureBox imgLoader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- EditorImagenes_Proyecto1/EditorImagenes_Proyecto1/Form1.cs
+++ EditorImagenes_Proyecto1/EditorImagenes_Proyecto1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,46 @@ namespace EditorImagenes_Proyecto1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            openFileDialog1.Filter = "Png files (*.png)|*.png|Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg";
+            openFileDialog1.Title = "Select an imagen file";
+
+            // Show the Dialog.  
+            // If the user clicked OK in the dialog and  
+            // a .CUR file was selected, open it.  
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                imgLoader1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+            /*if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                MessageBox.Show(sr.ReadToEnd());
+                sr.Close();
+            }*/
+            /*try
+            {
+                Bitmap image1 = (Bitmap)Image.FromFile(@"C:\Documents and Settings\" +
+                    @"All Users\Documents\My Music\music.bmp", true);
+
+                TextureBrush texture = new TextureBrush(image1);
+                texture.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
+                Graphics formGraphics = this.CreateGraphics();
+                formGraphics.FillEllipse(texture,
+                    new RectangleF(90.0F, 110.0F, 100, 100));
+                formGraphics.Dispose();
+
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("There was an error opening the bitmap." +
+                    "Please check the path.");
+            }*/
         }
     }
 }
