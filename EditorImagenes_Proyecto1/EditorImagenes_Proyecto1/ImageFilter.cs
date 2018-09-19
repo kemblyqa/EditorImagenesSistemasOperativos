fb2a127949b2b2a8d528a9e30243bc5f76f2dd0e
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-09-19 10:08:41 -0600

    Creación clase para los ajustes de imagen

---
+++ EditorImagenes_Proyecto1/EditorImagenes_Proyecto1/ImageFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorImagenes_Proyecto1
+{
+    class ImageFilter
+    {
+        public ImageFilter() { }
+
+        //grayscale
+
+        public Bitmap grayScale(Bitmap bitmap)
+        {
+            return bitmap;
+        }
+
+        //brigth
+        public Bitmap brigthness(Bitmap bitmap)
+        {
+            return bitmap;
+        }
+        //texture
+        public Bitmap texture(Bitmap bitmap)
+        {
+            return bitmap;
+        }
+    }
+}
